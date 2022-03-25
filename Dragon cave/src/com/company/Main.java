25fa5176a7548763d510969d19a624848f42f523
Dakarai <cenatusd23144@gmail.com>
2022-03-25 09:09:32 -0700

    Setting up the inital story for the project.

--- Dragon cave/src/com/company/Main.java
+++ Dragon cave/src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hi");
+        System.out.println("You are in a land full of Dragons in Front of you, ");
+        System.out.println("You see two Caves, Inn one Cave, the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and will eat you on sight.");
     }
 }
