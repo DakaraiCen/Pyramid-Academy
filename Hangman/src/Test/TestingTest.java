08d25dec4438d423fea660524906b9755cdb5a8c
Dakarai <cenatusd23144@gmail.com>
2022-04-10 12:05:36 -0700

    Finshed Project and Testing

---
+++ Hangman/src/Test/TestingTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestingTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+    @org.junit.jupiter.api.Test
+    void main() {
+        
+    }
+
+        @org.junit.jupiter.api.Test
+        void hang() {
+
+        }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+}
