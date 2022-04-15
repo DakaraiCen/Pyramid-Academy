04d854433bc15fc41189b2d06c33ce6f4332d7ac
Dakarai <cenatusd23144@gmail.com>
2022-04-15 14:08:38 -0700

    Started with HumanVSGoblins

---
+++ HumanVSGoblin/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean playAgain = true;
+
+        Scanner nameInput = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String setName = nameInput.nextLine().trim();
+
+    }
+}
