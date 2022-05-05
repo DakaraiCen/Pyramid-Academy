e798a4a8c89e506f9c0d8dcb16f7a9acf061c840
Dakarai <cenatusd23144@gmail.com>
2022-05-04 18:59:57 -0700

    Finshed Passenger

--- BoardingPass/src/Passenger.java
+++ BoardingPass/src/Passenger.java
@@ -12,5 +12,45 @@ public class Passenger {
         this.gender = gender;
         this.age = age;
     }
-       
+       public Passenger() {
+
+       }
+       public String getName() {
+        return name;
+       }
+       public void setName(String name){
+        this.name = name;
+       }
+
+       public String getEmail() {
+        return email;
+       }
+
+       public void setEmail(String email) {
+        this.email = email;
+       }
+
+       public String getPhone() {
+        return phone;
+       }
+
+       public void setPhone(String phone){
+        this.phone = phone;
+       }
+
+       public String getGender() {
+        return gender;
+       }
+
+       public void setGender(String gender){
+        this.gender = gender;
+       }
+
+       public int getAge() {
+        return age;
+       }
+
+       public void setAge(int age) {
+        this.age = age;
+       }
 }
